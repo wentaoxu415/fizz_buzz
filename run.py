20787ea7fa0f4b57aad579c7c629098301f70ad4
Wentao Xu <contactwentaoxu@gmail.com>
2015-12-12 23:22:31 -0800

    Fixed indentations

--- run.py
+++ run.py
@@ -9,18 +9,18 @@ def hello_monkey():
     resp = twilio.twiml.Response()
     resp.say("Hello Monkey")
  
- 	with resp.gather(timeout=10, finishOnKey="*", action="/handle-key", method="POST") as g:
- 		g.say("Please enter your number and then press star.")
+    with resp.gather(timeout=10, finishOnKey="*", action="/handle-key", method="POST") as g:
+        g.say("Please enter your number and then press star.")
 
     return str(resp)
  
- @app.route("/handle-key", methods=['GET', 'POST'])
- def handle_key():
- 	digit_pressed = request.values.get('Digits', None)
- 	
- 	resp = twilio.twiml.Response()
- 	resp.say("You've pressed " + digit_pressed)
- 	return str(resp)
+@app.route("/handle-key", methods=['GET', 'POST'])
+def handle_key():
+    digit_pressed = request.values.get('Digits', None)
+    
+    resp = twilio.twiml.Response()
+    resp.say("You've pressed " + digit_pressed)
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
