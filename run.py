6ccae25cb0febda959e3f3a1b1b0b46c4e06ca80
Wentao Xu <contactwentaoxu@gmail.com>
2015-12-13 21:15:47 -0800

    Validation is finally working

--- run.py
+++ run.py
@@ -9,23 +9,26 @@ app = Flask(__name__)
 def hello():
     """Respond to incoming requests."""
 
+    resp = twilio.twiml.Response()
     auth_token = '7b6c1a1b2e42c0dbb9204ed885cf5857' 
     validator = RequestValidator(auth_token)
     
-    if 'X-Twilio-Signature' in request.headers:        
+    if 'X-Twilio-Signature' not in request.headers:        
+        pass
+    else:         
         my_url = 'https://still-escarpment-3259.herokuapp.com'
         params = request.form
         twilio_signature = request.headers['X-Twilio-Signature']
-        
         if validator.validate(my_url, params, twilio_signature):
-            resp = twilio.twiml.Response()
             resp.say("Hello!")
-        
             with resp.gather(timeout=10, finishOnKey="*", action="/handle-key", method="POST") as g:
                 g.say("Please enter your number and then press star.")
-
-            return str(resp)
+        else: 
+            #raise AuthenticationFailed("Authentication Failed")
+            pass
+    return str(resp)
         
+
 @app.route("/handle-key", methods=['GET', 'POST'])
 def handle_key():
 
