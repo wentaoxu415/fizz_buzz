63621fe0b42a49c38a1e538dd634934f0335c4b1
Wentao Xu <contactwentaoxu@gmail.com>
2015-12-14 15:01:13 -0800

    Completed Phase 2

--- phase_2/local_settings.py
+++ phase_2/local_settings.py
@@ -4,14 +4,14 @@ Configuration Settings
 
 ''' Uncomment to configure using the file.  
 WARNING: Be careful not to post your account credentials on GitHub.
-
-TWILIO_ACCOUNT_SID = "ACxxxxxxxxxxxxx" 
-TWILIO_AUTH_TOKEN = "yyyyyyyyyyyyyyyy"
-TWILIO_CALLER_ID = "+17778889999"
 '''
+TWILIO_ACCOUNT_SID = "AC42eb3448aa02035843f457e6c9c2ed41" 
+TWILIO_AUTH_TOKEN = "7b6c1a1b2e42c0dbb9204ed885cf5857"
+TWILIO_CALLER_ID = "+12815423439"
+TWILIO_APP_SID = "PN6fc95ca26de7a521dcbc8374dcaec35b"
 
-import os
-TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
-TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
-TWILIO_CALLER_ID = os.environ.get('TWILIO_CALLER_ID', None)
-TWILIO_APP_SID = os.environ.get('TWILIO_APP_SID', None)
+# import os
+# TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
+# TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
+# TWILIO_CALLER_ID = os.environ.get('TWILIO_CALLER_ID', None)
+# TWILIO_APP_SID = os.environ.get('TWILIO_APP_SID', None)
